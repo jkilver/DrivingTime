d40987b04beef4504842b5da296c1f7ddeb08269
Jacob Kilver <robobookworm@gmail.com>
2017-06-29 22:33:41 -0400

    Take current driving conditions into account
    The API interface I was using at first I don't think took into account the
    real time driving conditions. I got an API key and can now get what I
    think are travel times based on real time conditions. This will give me
    the most accurate feedback

--- DrivingTime.py
+++ DrivingTime.py
@@ -6,10 +6,13 @@ import datetime as dt
 import matplotlib.pyplot as plt
 import time
 import calendar
+import numpy as np
 
-def SavePlot(sample_times, driving_times):
+def SavePlot(sample_times, to_work, from_work):
     # Create the dataframe
-    df = pd.DataFrame(driving_times, index=sample_times, columns=['driving time (minutes)'] )
+    driving_times = np.array([ to_work, from_work ]).transpose()
+
+    df = pd.DataFrame(driving_times, index=sample_times, columns=['driving time to work (minutes)', 'driving time from work (minutes)'] )
 
     # Plot the dataframe
     df.plot()
@@ -32,12 +35,18 @@ if __name__ == "__main__":
     dest_lat = 42.515091 
     dest_lng = -83.040376
 
+    # API Key
+    # AIzaSyDwGmIteahxi5ixK0P2aL055lIzhnMgvHA 
+
     # Generate the url
     url = "http://maps.googleapis.com/maps/api/distancematrix/json?origins={0},{1}&destinations={2},{3}&mode=driving&language=en-EN&sensor=false".format(str(orig_lat),str(orig_lng),str(dest_lat),str(dest_lng))
-
+    url_to_work = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=8929+Hough+Road+Almont+MI&destinations=Warren+Technical+Center&departure_time=now&key=AIzaSyDwGmIteahxi5ixK0P2aL055lIzhnMgvHA"
+    url_from_work = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Warren+Technical+Center&destinations=8929+Hough+Road+Almont+MI&departure_time=now&key=AIzaSyDwGmIteahxi5ixK0P2aL055lIzhnMgvHA"
+    
     # Allocate the arrays
     sample_times = []
-    driving_times = []
+    times_to_work = []
+    times_from_work = []
 
     prev_time = dt.datetime.now()
 
@@ -47,18 +56,22 @@ if __name__ == "__main__":
         sample_times.append(current)
 
         # Get the response from Google
-        result= simplejson.load(urllib.urlopen(url))
+        result_to_work = simplejson.load(urllib.urlopen(url_to_work))
+        result_from_work  = simplejson.load(urllib.urlopen(url_from_work))
 
         # Add the driving time to the list
-        driving_time = result['rows'][0]['elements'][0]['duration']['value']/60.0
-        driving_times.append(driving_time)
-        print "Driving time at " + str(current) + " is " + str(driving_time) + " minutes"
+        time_to_work = result_to_work['rows'][0]['elements'][0]['duration_in_traffic']['value']/60.0
+        time_from_work = result_from_work['rows'][0]['elements'][0]['duration_in_traffic']['value']/60.0
+        times_to_work.append(time_to_work)
+        times_from_work.append(time_from_work)
+        print "At time " + str(current) + " driving time to work is " + str(time_to_work) + " minutes. Driving time from work is " + str(time_from_work)
 
         # Determine if we need to switch the data sets over and save off the plots
         if current.day != prev_time.day:
-            SavePlot(sample_times, driving_times)
+            SavePlot(sample_times, times_to_work, times_from_work)
             sample_times[:] = []
-            driving_times[:] = []
+            times_to_work[:] = []
+            times_from_work[:] = []
 
         # Update the previous time variable  
         prev_time = current
